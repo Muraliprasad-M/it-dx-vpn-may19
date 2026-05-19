aa1d81fa2f50a8526010c0fd0bfa9e7b6c5e4bc0
Muraliprasad-M <MuraliPrasad.M@cognizant.com>
2026-05-19 19:43:36 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,19 @@
-output "dx_gateway_id" { value = aws_dx_gateway.dxgw.id }
-output "transit_vif_id" { value = aws_dx_transit_virtual_interface.transit_vif.id }
+output "dx_gateway_id" {
+  description = "DX Gateway ID"
+  value       = aws_dx_gateway.dxgw.id
+}
+
+output "dx_gateway_association_id" {
+  description = "DXGW to TGW association ID"
+  value       = aws_dx_gateway_association.dxgw_tgw.id
+}
+
+output "transit_vif_ids" {
+  description = "Transit VIF IDs"
+  value       = aws_dx_transit_virtual_interface.transit_vif[*].id
+}
+
+output "transit_vif_names" {
+  description = "Transit VIF Names"
+  value       = aws_dx_transit_virtual_interface.transit_vif[*].name
+}
