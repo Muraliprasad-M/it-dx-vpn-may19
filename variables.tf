741baf428e5ab46b75b085be7245d784249df4c2
Muraliprasad-M <MuraliPrasad.M@cognizant.com>
2026-05-19 19:37:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,59 @@
-variable "region" { default = "eu-west-2" }
-variable "network_account_role" {}
-variable "dx_gateway_name" { default = "central-dxgw" }
-variable "amazon_side_asn" { default = 64512 }
-variable "tgw_id" {}
-variable "dx_connection_id" {}
-variable "vlan" {}
-variable "onprem_bgp_asn" {}
-variable "amazon_ip" {}
-variable "customer_ip" {}
-variable "allowed_prefixes" { type = list(string) default = ["10.0.0.0/8"] }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "network_account_role" {
+  description = "Control Tower execution role in Network account"
+  type        = string
+}
+
+variable "dx_gateway_name" {
+  description = "Direct Connect Gateway name"
+  type        = string
+  default     = "central-dxgw"
+}
+
+variable "amazon_side_asn" {
+  description = "ASN for DX Gateway"
+  type        = number
+  default     = 64512
+}
+
+variable "tgw_id" {
+  description = "Existing Transit Gateway ID"
+  type        = string
+}
+
+# HA Inputs
+variable "dx_connection_ids" {
+  description = "Two DX connection IDs (for HA across locations)"
+  type        = list(string)
+}
+
+variable "vlans" {
+  description = "VLAN IDs per VIF"
+  type        = list(number)
+}
+
+variable "amazon_ips" {
+  description = "Amazon side IPs per VIF (/30)"
+  type        = list(string)
+}
+
+variable "customer_ips" {
+  description = "Customer side IPs per VIF (/30)"
+  type        = list(string)
+}
+
+variable "onprem_bgp_asn" {
+  description = "On-prem BGP ASN"
+  type        = number
+}
+
+variable "allowed_prefixes" {
+  description = "Allowed AWS CIDR prefixes advertised to on-prem"
+  type        = list(string)
+  default     = ["10.0.0.0/8"]
+}
